75d4e66b7e43f5e7543b567062c26037a755dffd
Aras Pranckevicius <aras@nesnausk.org>
2025-02-02 22:51:57 +0200

    Misc: fix emscripten compile

--- src/platform.c
+++ src/platform.c
@@ -657,14 +657,6 @@ static void sapp_frame(void)
 {
 	app_update();
 
-	#if defined(__EMSCRIPTEN__)
-	if (saudio_suspended()) {
-		if (0 == (sapp_frame_count() & (1<<5))) {
-			draw_text("click/tap to start", (400 - (18*8)) / 2, (240 - 7) / 2);
-		}
-	}
-	#endif
-
 	sg_update_image(sok_image, &(sg_image_data){.subimage[0][0] = SG_RANGE(s_screen_buffer)});
 	sg_update_image(sok_debug_image, &(sg_image_data){.subimage[0][0] = SG_RANGE(s_screen_debug_buffer)});
 
